0e9dfb7a49628a8d0fc2dfb2e6f9b076ec659fe2
Alejandro Perez <aperef04@estudiantes.unileon.es>
2021-05-30 14:02:49 +0200

    Ya se muestran correctamente los cromos

--- Administrador/admin.js
+++ Administrador/admin.js
@@ -86,7 +86,7 @@ function pintarCromos(vectorCromos) {
   console.log(vectorCromos);
   for(let i =0;i< vectorCromos.length ; i++ ){
     let j = i+1;
-    /*string += '<div class="u-align-left u-container-style u-list-item u-repeater-item u-shape-rectangle"> <div class="u-container-layout u-similar-container u-container-layout-' +1+ '">'+
+    string += '<div class="u-align-left u-container-style u-list-item u-repeater-item u-shape-rectangle"> <div class="u-container-layout u-similar-container u-container-layout-' +1+ '">'+
         '<div class="u-clearfix u-custom-html u-custom-html-' + '1'+'">'+
         '<div class="cromo"> <div class="decoracion"> <div class="top"> <div class="nombreCromo">  <h3>'+ vectorCromos[i].Nombre +'</h3></div>'+
         '<div class="border"><img src="'+ vectorCromos[i].RutaImagen +'" alt="imagen"></div></div>'+
@@ -100,9 +100,7 @@ function pintarCromos(vectorCromos) {
         '</a> <a href="" class="u-btn u-btn-round u-button-style u-custom-color-4 u-radius-4 u-text-body-alt-color u-btn-'+'1'+'">'+
         '<span class="u-icon u-icon-'+'1'+'"><svg class="u-svg-content" viewBox="0 0 492.49284 492" style="width: 1em; height: 1em;"><path d="m304.140625 82.472656-270.976563 270.996094c-1.363281 1.367188-2.347656 3.09375-2.816406 4.949219l-30.035156 120.554687c-.898438 3.628906.167969 7.488282 2.816406 10.136719 2.003906 2.003906 4.734375 3.113281 7.527344 3.113281.855469 0 1.730469-.105468 2.582031-.320312l120.554688-30.039063c1.878906-.46875 3.585937-1.449219 4.949219-2.8125l271-270.976562zm0 0"></path><path d="m476.875 45.523438-30.164062-30.164063c-20.160157-20.160156-55.296876-20.140625-75.433594 0l-36.949219 36.949219 105.597656 105.597656 36.949219-36.949219c10.070312-10.066406 15.617188-23.464843 15.617188-37.714843s-5.546876-27.648438-15.617188-37.71875zm0 0"></path></svg><img></span>&nbsp;BORRAR'+
         '</a></div></div>';
-    */
 
   }
-  string+="";
   document.getElementById("zonaCromos").innerHTML = string;  
 }
