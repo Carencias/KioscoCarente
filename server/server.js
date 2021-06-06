4bbf614edff824b65b32e6418c2a50d00122229e
Santi <47595372+svalbr00@users.noreply.github.com>
2021-06-06 13:06:20 +0200

    Intentado arreglar pagina cliente
    hay que sacar el render del for each para que no se renderice la pagina varias veces

--- server/server.js
+++ server/server.js
@@ -419,6 +419,47 @@ app.get("/dashboard/user", async function (req, res) {
   });
 });
 
+/*Intento de fumada, tambien intente con awit,  hacer otra funcion no se, hay que sacar el render del for each
+//PAGINA PRINCIPAL USUARIO
+app.get("/dashboard/user", async function (req, res) {
+  let string = "SELECT * FROM ALBUMES WHERE User = '" + req.session.user + "'";
+  var colecciones = [];
+  let estadosAlbumes = [];
+  connection.query(string, function (err, result, fields) {
+    if (err) {
+      lanzarError(res,"Error al consultar la base de datos");
+    }
+
+    new Promise(function (result) {
+      result.forEach(function (album) {
+        estadosAlbumes.push(album.Estado);
+        obtenerColecciones(album.Coleccion).then(function (coleccionesBBDD) {
+        colecciones.push(coleccionesBBDD[0]);
+        console.log(coleccionesBBDD[0])
+      }, (error) => {
+        lanzarError(res,"Error de datos");
+      });
+    });
+    }).then( function(estadosAlbumes, colecciones) {
+        let stringUser = "SELECT * FROM CLIENTES WHERE User = '" + req.session.user + "'";
+       
+        connection.query(stringUser, function (err, resultUser, fields) {
+          if (err) {
+            lanzarError(res," al consultar la base de datos");
+          }
+          res.render('user/clientePrincipal', {
+            colecciones: colecciones,
+            estados: estadosAlbumes,
+            nombre: req.session.user,
+            puntos: resultUser[0].Puntos
+          });
+        });
+      }, (error) => {
+        lanzarError(res," asdal consultar la base de datos");
+    });
+  });
+});*/
+
 //TIENDA USUARIO ALBUMES
 app.get("/dashboard/user/tiendaAlbumes", function (req, res) {
 
