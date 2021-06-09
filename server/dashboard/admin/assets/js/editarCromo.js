36823e55f8c1e46176d0ea7f19accf8df494f922
Marcos Ferreras Rodríguez <mferrr03@estudiantes.unileon.es>
2021-06-09 15:17:53 +0200

    Corregido editar cromo dinamico.
    Ahora funciona, y se pueden deshacer los cambios.

--- server/dashboard/admin/assets/js/editarCromo.js
+++ server/dashboard/admin/assets/js/editarCromo.js
@@ -1,32 +1,32 @@
-var formulario_editar_cromo = {}, cromo_editar_cromo = {};
 
-function cargarDatosFomularioEditarCromo(){
-    formulario_editar_cromo.nombre = document.getElementById("formulario_editar_cromo_nombre").value;
-    formulario_editar_cromo.imagen = document.getElementById("formulario_editar_cromo_imagen").value;
-    formulario_editar_cromo.stock = document.getElementById("formulario_editar_cromo_stock").value;
-    formulario_editar_cromo.descripcion = document.getElementById("formulario_editar_cromo_descripcion").value;
-    formulario_editar_cromo.dato = document.getElementById("formulario_editar_cromo_dato").value;
-    formulario_editar_cromo.frecuencia = document.getElementById("formulario_editar_cromo_frecuencia").value;
+
+const rutaImagen = document.getElementById("imagen_cromo").src;
+
+function editarCromo() {
+
+    setTimeout(() =>{
+
+        document.getElementById("descripcion_cromo").innerHTML = document.getElementById("formulario_editar_cromo_descripcion").value;
+        document.getElementById("dato_cromo").innerHTML = document.getElementById("formulario_editar_cromo_dato").value;
+        document.getElementById("frecuencia_cromo").innerHTML = document.getElementById("formulario_editar_cromo_frecuencia").value;
+
+    } , 300);
+
 }
 
-function cargarDatosCromoEditar(){
-    inicializarCamposCromoEditar();
-    cargarDatosFomularioEditarCromo();
-    cromo_editar_cromo.nombre.innerHTML = formulario_editar_cromo.nombre;
-    cromo_editar_cromo.imagen.source = formulario_editar_cromo.imagen;
-    cromo_editar_cromo.descripcion.innerHTML = formulario_editar_cromo.descripcion;
-    cromo_editar_cromo.dato.innerHTML = "\""+formulario_editar_cromo.dato+"\"";
-    cromo_editar_cromo.frecuencia.innerHTML = formulario_editar_cromo.frecuencia;
-    let url = window.location.search;
-    const urlParams = new URLSearchParams(url);
-    cromo_editar_cromo.lenguaje.innerHTML = urlParams.get('nombreColeccion');
-}
+function editarImagen(file) {
 
-function inicializarCamposCromoEditar(){
-    cromo_editar_cromo.nombre = document.getElementById("nombre_cromo");
-    cromo_editar_cromo.imagen = document.getElementById("imagen_cromo");
-    cromo_editar_cromo.descripcion = document.getElementById("descripcion_cromo");
-    cromo_editar_cromo.dato = document.getElementById("dato_cromo");
-    cromo_editar_cromo.frecuencia = document.getElementById("frecuencia_cromo");
-    cromo_editar_cromo.lenguaje = document.getElementById("lenguaje_cromo");
-}
+    var input = file.target;
+
+    var reader = new FileReader();
+    reader.onload = function () {
+        var dataURL = reader.result;
+        var output = document.getElementById("imagen_cromo");
+        output.src = dataURL;
+    };
+    reader.readAsDataURL(input.files[0]);
+};
+
+function restaurarImagen() {
+    document.getElementById("imagen_cromo").setAttribute("src", rutaImagen);
+};
