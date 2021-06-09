20b0ceca83fd8cf103cd3169a8b30d70a5fab4df
cdiezg07 <cdiezg07@estudiantes.unileon.es>
2021-06-09 17:52:34 +0200

    Formulario dinámico EditarColeccion

--- server/server.js
+++ server/server.js
@@ -351,17 +351,25 @@ app.get("/dashboard/resources/*", function (req, res) {
 app.get("/dashboard/admin/editarColeccion", function (req, res) {
   //TODO comprobar entrada??
 
-  let coleccion = req.query.nombreColeccion;
+  let nomColeccion = req.query.nombreColeccion;
 
-  let string = "SELECT * FROM CROMOS WHERE Coleccion = '" + coleccion + "'";
+  let string = "SELECT * FROM CROMOS WHERE Coleccion = '" + nomColeccion + "'";
   connection.query(string, function (err, result, fields) {
     if (err) {
       lanzarError(res, "Error al consultar la base de datos");
     }
-    res.render('admin/administradorEditarColeccion', {
-      cromos: result,
-      nombreColeccion: coleccion
+
+    obtenerColecciones(nomColeccion).then(function (coleccion) {
+      
+      res.render('admin/administradorEditarColeccion', {
+        cromos: result,
+        Coleccion: coleccion[0]
+      });
+      
+    }, function (error) {
+      lanzarError(res, "Error al consultar la base de datos");
     });
+    
 
   });
 
